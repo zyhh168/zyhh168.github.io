411b2c23d5414781c65f2730cdbf825132e5ec70
zyhh168 <771817997@qq.com>
2018-06-07 18:50:30 +0800

    css & js
    css & js

---
+++ source1/js/audio.js
@@ -0,0 +1,75 @@
+/* play */
+function blkAutoplay(){ 
+	blk.autoplay = true;
+	blk.load();
+}
+function qbAutoplay(){ 
+	qb.autoplay = true;
+	qb.load();
+}
+function wspAutoplay(){ 
+	wsp.autoplay = true;
+	wsp.load();
+}
+function slAutoplay(){ 
+	sl.autoplay = true;
+	sl.load();
+}
+function lfAutoplay(){ 
+	lf.autoplay = true;
+	lf.load();
+}
+function nmAutoplay(){ 
+	nm.autoplay = true;
+	nm.load();
+}
+function szAutoplay(){ 
+	sz.autoplay = true;
+	sz.load();
+}
+function flqAutoplay(){ 
+	flq.autoplay = true;
+	flq.load();
+}
+function lbAutoplay(){ 
+	lb.autoplay = true;
+	lb.load();
+}
+
+/* stop */
+function blkstop(){ 
+	blk.autoplay = false;
+	blk.load();
+}
+function qbstop(){ 
+	qb.autoplay = false;
+	qb.load();
+}
+function wspstop(){ 
+	wsp.autoplay = false;
+	wsp.load();
+}
+function slstop(){ 
+	sl.autoplay = false;
+	sl.load();
+}
+function lfstop(){ 
+	lf.autoplay = false;
+	lf.load();
+}
+function nmstop(){ 
+	nm.autoplay = false;
+	nm.load();
+}
+function szstop(){ 
+	sz.autoplay = false;
+	sz.load();
+}
+function flqstop(){ 
+	flq.autoplay = false;
+	flq.load();
+}
+function lbstop(){ 
+	lb.autoplay = false;
+	lb.load();
+}
